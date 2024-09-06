168bc3575805024431e8aafaab5d8352f32e4fd6
Bayraa Kaspersky <36721957+BayraaKaspersky@users.noreply.github.com>
2024-09-06 02:02:07 +0000

    update

---
+++ Filter Elements from Array/Filter Elements from Array.js
@@ -0,0 +1,18 @@
+// Source : https://leetcode.com/problems/filter-elements-from-array/description/
+// Author : Bayarjargal
+// Date   : 2024-09-06
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function (arr, fn) {
+  let filteredArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      filteredArr.push(arr[i]);
+    }
+  }
+  return filteredArr;
+};
