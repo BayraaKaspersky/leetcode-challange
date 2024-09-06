2f5120a6c71154c2e14eaa9204ca401f88889bd3
Bayraa Kaspersky <36721957+BayraaKaspersky@users.noreply.github.com>
2024-09-06 01:59:52 +0000

    update

---
+++ Apply Transform Over Each Element in Array/Apply Transform Over Each Element in Array.js
@@ -0,0 +1,18 @@
+// Source : https://leetcode.com/problems/array-reduce-transformation/
+// Author : Bayarjargal
+// Date   : 2024-09-06
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+
+var reduce = function (nums, fn, init) {
+  let val = init;
+  for (let i = 0; i < nums.length; i++) {
+    val = fn(val, nums[i]);
+  }
+  return val;
+};
