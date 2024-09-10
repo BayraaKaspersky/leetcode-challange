7d523d18ebdbc73a30cd1a866e634f6a721d3d8e
Bayraa Kaspersky <36721957+BayraaKaspersky@users.noreply.github.com>
2024-09-10 00:03:31 +0000

    update

---
+++ Find Peak Element/Find Peak Element.js
@@ -0,0 +1,22 @@
+// Source : https://leetcode.com/problems/find-peak-element/description/
+// Author : Bayarjargal
+// Date   : 2024-09-10
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] > nums[mid + 1]) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
+};
