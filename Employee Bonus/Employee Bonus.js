c8e12dd5bfa1ea4f83bd440e0aa692a72e3ea168
Bayarjargal Basbayar <bayraakaspersky@Bayarjargals-MacBook-Pro.local>
2024-09-16 13:01:03 +0900

    update

---
+++ Employee Bonus/Employee Bonus.js
@@ -0,0 +1,16 @@
+// Source : https://leetcode.com/problems/employee-bonus/description/
+// Author : Bayarjargal
+// Date   : 2024-09-16
+
+# Write your MySQL query statement below
+SELECT 
+    e.name,
+    b.bonus
+FROM 
+    Employee e
+LEFT JOIN 
+    Bonus b
+ON 
+    e.empId = b.empId
+WHERE 
+    b.bonus IS NULL OR b.bonus < 1000;
