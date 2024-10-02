11d7815dc3cd325551628dec88b345698038da33
Bayarjargal <36721957+BayraaKaspersky@users.noreply.github.com>
2024-10-02 09:00:52 +0900

    update

---
+++ Ugly Number.js
@@ -0,0 +1,18 @@
+// Source : https://leetcode.com/problems/ugly-number/description
+// Author : Bayarjargal
+// Date   : 2024-10-02
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function(n) {
+    if (n <= 0) return false
+
+    while (n % 2 == 0) n /= 2
+    while (n % 3 == 0) n /= 3
+    while (n % 5 == 0) n /= 5
+
+    return n == 1
+   
+};
