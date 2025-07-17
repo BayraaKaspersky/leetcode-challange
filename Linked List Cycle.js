e7480e6f84af37569b305b7e7cf3a51a2acc5f2b
Bayarjargal <36721957+BayraaKaspersky@users.noreply.github.com>
2025-07-17 11:04:43 +0900

    update

--- Linked List Cycle.js
+++ Linked List Cycle.js
@@ -1,4 +1,4 @@
-// Source : https://leetcode.com/problems/linked-list-cycle
+// Source : https://leetcode.com/problems/linked-list-cycle/description
 // Author : Bayarjargal
 // Date   : 2025-07-17
 
